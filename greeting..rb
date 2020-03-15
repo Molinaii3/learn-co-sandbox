75b009024be9f5efb2bf31190d76e12b58053b3a
Michael Molina <l.13molina@gmail.com>
2020-03-15 22:56:46 +0000

    Automatically backed up by Learn

---
+++ greeting..rb
@@ -0,0 +1,15 @@
+ def greeting
+  puts "Hi, Ruby programmer!"
+end
+
+def greeting_python
+  puts "Hello, Python programmer!"
+end
+
+def greeting_a_person(Sophie)
+  "Hello #{Sophie}"
+end
+
+def print_one_plus_one
+  puts 1+1
+end
